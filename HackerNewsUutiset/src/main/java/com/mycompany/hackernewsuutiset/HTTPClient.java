6cfa38ca75e2cc014baa0850e63b99ef46cf30d5
Pihla Toivanen <toivanenpihla@gmail.com>
2015-08-22 22:50:24 +0300

    http clientti

---
+++ HackerNewsUutiset/src/main/java/com/mycompany/hackernewsuutiset/HTTPClient.java
@@ -0,0 +1,40 @@
+
+package com.mycompany.hackernewsuutiset;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+
+
+public class HTTPClient {
+    public static String callURL(String URLString) {
+		StringBuilder sb = new StringBuilder();
+		URLConnection urlConn = null;
+		InputStreamReader in = null;
+		try {
+			URL url = new URL(URLString);
+			urlConn = url.openConnection();
+			if (urlConn != null)
+				urlConn.setReadTimeout(60 * 1000);
+			if (urlConn != null && urlConn.getInputStream() != null) {
+				in = new InputStreamReader(urlConn.getInputStream(),
+						Charset.defaultCharset());
+				BufferedReader bufferedReader = new BufferedReader(in);
+				if (bufferedReader != null) {
+					int cp;
+					while ((cp = bufferedReader.read()) != -1) {
+						sb.append((char) cp);
+					}
+					bufferedReader.close();
+				}
+			}
+		in.close();
+		} catch (Exception e) {
+			throw new RuntimeException("Virhe hakiessa osoitteesta: "+ URLString, e);
+		} 
+ 
+		return sb.toString();
+	}
+}
--- HackerNewsUutiset/src/main/java/com/mycompany/hackernewsuutiset/HackerPaivanUutiset.java
+++ HackerNewsUutiset/src/main/java/com/mycompany/hackernewsuutiset/HackerPaivanUutiset.java
@@ -7,12 +7,16 @@ public class HackerPaivanUutiset implements PaivanUutiset {
 
     @Override
     public String haeSatunnainenUutinen() {
-        
+        String vastaus = HTTPClient.callURL("hs.fi");
+        System.out.println("Vastaus: " + vastaus);
+        return vastaus;
     }
 
     @Override
     public String haeLyhytUutinen() {
-        
+        String vastaus = HTTPClient.callURL("hs.fi");
+        System.out.println("Vastaus: " + vastaus);
+        return vastaus;
     }
     
 }
