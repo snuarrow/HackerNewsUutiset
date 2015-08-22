a594d3566d2cc63864b69170fcc525f6cbb48abb
Pihla Toivanen <toivanenpihla@gmail.com>
2015-08-22 22:42:14 +0300

    base

---
+++ HackerNewsUutiset/src/main/java/com/mycompany/hackernewsuutiset/HackerPaivanUutiset.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.hackernewsuutiset;
+
+import com.mycompany.paivanuutiset.PaivanUutiset;
+
+public class HackerPaivanUutiset implements PaivanUutiset {
+
+    @Override
+    public String haeSatunnainenUutinen() {
+        
+    }
+
+    @Override
+    public String haeLyhytUutinen() {
+        
+    }
+    
+}
