04a47803445365572e652227ebda7a440d3e2457
Pihla Toivanen <toivanenpihla@gmail.com>
2015-08-22 23:12:53 +0300

    .

--- HackerNewsUutiset/src/main/java/com/mycompany/hackernewsuutiset/HackerPaivanUutiset.java
+++ HackerNewsUutiset/src/main/java/com/mycompany/hackernewsuutiset/HackerPaivanUutiset.java
@@ -7,14 +7,14 @@ public class HackerPaivanUutiset implements PaivanUutiset {
 
     @Override
     public String haeSatunnainenUutinen() {
-        String vastaus = HTTPClient.callURL("hs.fi");
+        String vastaus = HTTPClient.callURL("http://hs.fi");
         System.out.println("Vastaus: " + vastaus);
         return vastaus;
     }
 
     @Override
     public String haeLyhytUutinen() {
-        String vastaus = HTTPClient.callURL("hs.fi");
+        String vastaus = HTTPClient.callURL("http://hs.fi");
         System.out.println("Vastaus: " + vastaus);
         return vastaus;
     }
