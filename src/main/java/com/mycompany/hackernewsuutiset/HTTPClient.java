39b38b17888e53ff4293cee7ed764075d36f9ccf
snuarrow <henri.vaara@helsinki.fi>
2015-09-09 21:42:21 +0300

    teht7 method refactor

--- src/main/java/com/mycompany/hackernewsuutiset/HTTPClient.java
+++ src/main/java/com/mycompany/hackernewsuutiset/HTTPClient.java
@@ -2,6 +2,7 @@
 package com.mycompany.hackernewsuutiset;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
@@ -16,24 +17,30 @@ public class HTTPClient {
 		try {
 			URL url = new URL(URLString);
 			urlConn = url.openConnection();
-			if (urlConn != null)
-				urlConn.setReadTimeout(60 * 1000);
-			if (urlConn != null && urlConn.getInputStream() != null) {
-				in = new InputStreamReader(urlConn.getInputStream(),
-						Charset.defaultCharset());
-				BufferedReader bufferedReader = new BufferedReader(in);
-				if (bufferedReader != null) {
-					int cp;
-					while ((cp = bufferedReader.read()) != -1) {
-						sb.append((char) cp);
-					}
-					bufferedReader.close();
-				}
-			}
-		in.close();
+                        in = connectA(urlConn, in, sb);
+                        in.close();
 		} catch (Exception e) {
 			throw new RuntimeException("Virhe hakiessa osoitteesta: "+ URLString, e);
 		} 
 		return sb.toString();
 	}
+
+    private static InputStreamReader connectA(URLConnection urlConn, InputStreamReader in, StringBuilder sb) throws IOException
+    {
+        if (urlConn != null)
+            urlConn.setReadTimeout(60 * 1000);
+        if (urlConn != null && urlConn.getInputStream() != null) {
+            in = new InputStreamReader(urlConn.getInputStream(),
+                    Charset.defaultCharset());
+            BufferedReader bufferedReader = new BufferedReader(in);
+            if (bufferedReader != null) {
+                int cp;
+                while ((cp = bufferedReader.read()) != -1) {
+                    sb.append((char) cp);
+                }
+                bufferedReader.close();
+            }
+			}
+        return in;
+    }
 }
